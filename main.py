1558b62af16dcf8a85d154ffc7c4fe660160cb81
Akshaya H <81792372+my-my414@users.noreply.github.com>
2021-09-04 10:21:25 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    float gpa;
+    printf("Enter your GPA :");
+    scanf("%f",&gpa);
+    if(gpa<=5)
+    {
+        printf("not eligible");
+    }
+    else
+    {
+        printf("eligible");
+    }
+     
+    
+}
